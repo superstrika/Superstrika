cd22bb61ed5b6d133fa3ebb5264f4fc6d9aad894
superstrika <superstrika7046@gmail.com>
2026-01-04 15:39:20 +0200

    Feat: added motor movement

--- src/my_robot_package/my_robot_package/motor.py
+++ src/my_robot_package/my_robot_package/motor.py
@@ -6,11 +6,18 @@ import random
         
     
 class dcmotdriver:
-    def __init__(self, in1_pin, in2_pin):
+    def __init__(self, in1_pin, in2_pin, switch: bool = False):
+
+        if switch:
+            in1_pin, in2_pin = in2_pin, in1_pin
+
         self.mot1 = PWM7046(in1_pin, 800)
         self.mot2 = PWM7046(in2_pin, 800)
+            
         self.mot1.value = 0  # start PWM with 0% duty cycle
         self.mot2.value = 0
+        self.AXIS_ROTATION = 45 # deg
+
         
 
     def motgo(self, speed):
@@ -40,19 +47,48 @@ class dcmotdriver:
         self.mot1.value = 0
         self.mot2.value = 0
 
+    def calculate_speed(self, Vx, Vy, rotation):
+        rad = math.radians(self.AXIS_ROTATION)
+        cos = math.cos(rad)
+        sin = math.sin(rad)
+        
+        # Standard 2D rotation matrix
+        target_Vx = Vx * cos - Vy * sin
+        target_Vy = Vx * sin + Vy * cos
+
+        # 2. Combine components for each wheel
+        # Each wheel gets a mix of Vx, Vy, and Rotation
+        wheel1_speed = target_Vy + target_Vx + rotation # Front Left
+        wheel2_speed = target_Vy - target_Vx - rotation # Front Right
+        wheel3_speed = target_Vy - target_Vx + rotation # Rear Left
+        wheel4_speed = target_Vy + target_Vx - rotation # Rear Right
+
+        # 3. Normalization (Crucial!)
+        # Since adding Vx + Vy + Rot can exceed 100, we must scale down
+        speeds = [wheel1_speed, wheel2_speed, wheel3_speed, wheel4_speed]
+        max_val = max(list(map(abs, speeds)) + [100])
+        
+        return [(s / max_val) * 100 for s in speeds]
+
 
 if __name__ == "__main__":
-    motor1 = dcmotdriver(19, 20)
-    motor2 = dcmotdriver(21, 22)
-    motor3 = dcmotdriver(23, 24)
-    motor4 = dcmotdriver(25, 26)
+    motor1 = dcmotdriver(19, 20, True) # green
+    motor2 = dcmotdriver(21, 22) # white
+    motor3 = dcmotdriver(23, 24, True) # orange
+    motor4 = dcmotdriver(25, 26) # orange white
      
-    motor2.motgo(30)
-    motor1.motgo(-30)
+    # motor2.motgo(30)
+    # motor3.motgo(100)
+    motors: dcmotdriver = [motor3, motor1, motor2, motor4]
+    speeds = motor1.calculate_speed(100, 0, 0)
+    print(speeds)
+    for i in range(len(speeds)):
+        motors[i].motgo(speeds[i])
+        
 
     input()
 
-    del motor, motor2, motor3, motor4
+    del motor1, motor2, motor3, motor4
 
     
  
