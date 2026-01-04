aa0576f6e58e0858d6870d8941b687748cb1ce25
Noam Ron <146370694+NoamRon1@users.noreply.github.com>
2026-01-04 21:50:43 +0200

    Feat: added better repo for motor: with properties

--- src/my_robot_package/my_robot_package/motor.py
+++ src/my_robot_package/my_robot_package/motor.py
@@ -1,89 +1,92 @@
 from pwm7046 import PWM7046
-import time 
 import math
-import random
 
         
     
-class dcmotdriver:
-    def __init__(self, in1_pin, in2_pin, switch: bool = False):
+class motor7046:
+    def __init__(self, pin1, pin2, switch: bool = False):
 
         if switch:
-            in1_pin, in2_pin = in2_pin, in1_pin
+            pin1, pin2 = pin2, pin1
 
-        self.mot1 = PWM7046(in1_pin, 800)
-        self.mot2 = PWM7046(in2_pin, 800)
-            
+        self.mot1 = PWM7046(pin1, 800)
+        self.mot2 = PWM7046(pin2, 800)
+
+        self._speed = 0
         self.mot1.value = 0  # start PWM with 0% duty cycle
         self.mot2.value = 0
-        self.AXIS_ROTATION = 45 # deg
 
-        
+    @property
+    def speed(self):
+        return self._speed
 
-    def motgo(self, speed):
-        pwm_value = abs(speed
-                        ) #convert to%
-        #print('speed pwm value = ', pwm_value)
-        if speed > 0:
+    @speed.setter
+    def speed(self, speed):
+        self._speed = speed
+        pwm_value = abs(self._speed)
+
+        if self._peed > 0:
             self.mot1.value = pwm_value
             self.mot2.value = 0
-        elif speed < 0:
+        elif self._speed < 0:
             self.mot1.value = 0
             self.mot2.value = pwm_value
         else:
             self.mot1.value = 0
-            self.mot2.value = 0    
+            self.mot2.value = 0
 
-    
     def stophard(self):
+        self._speed = 1
         self.mot1.value = 1
         self.mot2.value = 1
 
     def stop(self):
-        self.mot1.value = 0
-        self.mot2.value = 0
-    
-    def __del__(self):
+        self._speed = 0
         self.mot1.value = 0
         self.mot2.value = 0
 
-    def calculate_speed(self, Vx, Vy, rotation):
-        rad = math.radians(self.AXIS_ROTATION)
+    def __del__(self):
+        self._speed = 0
+        self.mot1.value = 0
+        self.mot2.value = 0
+
+    @staticmethod
+    def calculate_speed(Vx, Vy, rotation):
+        AXIS_ROTATION = 45
+        rad = math.radians(AXIS_ROTATION)  # Or self.AXIS_ROTATION
         cos = math.cos(rad)
         sin = math.sin(rad)
-        
-        # Standard 2D rotation matrix
-        target_Vx = Vx * cos - Vy * sin
-        target_Vy = Vx * sin + Vy * cos
 
-        # 2. Combine components for each wheel
-        # Each wheel gets a mix of Vx, Vy, and Rotation
-        wheel1_speed = target_Vy + target_Vx + rotation # Front Left
-        wheel2_speed = target_Vy - target_Vx - rotation # Front Right
-        wheel3_speed = target_Vy - target_Vx + rotation # Rear Left
-        wheel4_speed = target_Vy + target_Vx - rotation # Rear Right
+        # Standard rotation matrix to find the 'Wheel-Aligned' components
+        rotatedVx = Vx * cos - Vy * sin
+        rotatedVy = Vx * sin + Vy * cos
 
-        # 3. Normalization (Crucial!)
-        # Since adding Vx + Vy + Rot can exceed 100, we must scale down
+        # 2. Assign to wheels based on the X-pattern
+        # In an X-drive, the diagonal pairs handle the rotated vectors
+        wheel1_speed = rotatedVy + rotation  # Front Left
+        wheel2_speed = rotatedVx - rotation  # Front Right
+        wheel3_speed = rotatedVx + rotation  # Rear Left
+        wheel4_speed = rotatedVy - rotation  # Rear Right
+
+        # 3. Normalization (Scaling)
+        # This ensures that if the math results in '141', it scales back to '100'
         speeds = [wheel1_speed, wheel2_speed, wheel3_speed, wheel4_speed]
         max_val = max(list(map(abs, speeds)) + [100])
-        
+
         return [(s / max_val) * 100 for s in speeds]
 
 
 if __name__ == "__main__":
-    motor1 = dcmotdriver(19, 20, True) # green
-    motor2 = dcmotdriver(21, 22) # white
-    motor3 = dcmotdriver(23, 24, True) # orange
-    motor4 = dcmotdriver(25, 26) # orange white
-     
-    # motor2.motgo(30)
-    # motor3.motgo(100)
-    motors: dcmotdriver = [motor3, motor1, motor2, motor4]
-    speeds = motor1.calculate_speed(100, 0, 0)
+    motor1 = motor7046(19, 20, True) # green
+    motor2 = motor7046(21, 22) # white
+    motor3 = motor7046(23, 24, True) # orange
+    motor4 = motor7046(25, 26) # orange white
+
+    motors: motor7046 = [motor3, motor1, motor2, motor4]
+    speeds = motor1.calculate_speed(0, 100, 0)
     print(speeds)
     for i in range(len(speeds)):
-        motors[i].motgo(speeds[i])
+        motors[i].setSpeed(speeds[i])
         
 
     input()
