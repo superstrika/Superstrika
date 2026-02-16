694d6ef508e00257002558585292e4724f92725a
Noam Ron <Noam2009r@gmail.com>
2026-02-16 19:52:05 +0200

    Feat: added multiple TCRT: multithread calibration

--- src/my_robot_package/my_robot_package/TCRT5000.py
+++ src/my_robot_package/my_robot_package/TCRT5000.py
@@ -1,9 +1,9 @@
 import lgpio
 import input7046
 from time import sleep
+import threading
 
 class TCRT5000:
-    
     def __init__(self, pin, chipID=0):
         self._sensor = input7046.Input7046(pin, chipID=chipID)
         self._Field_value: int = None
@@ -32,9 +32,37 @@ class TCRT5000:
     def value(self):
         return self.read()
 
-if __name__ == "__main__":
-    s = TCRT5000(4)
-    s.calibrate()
+class multipleTCRT5000:
+    def __init__(self, pins: list[int], chipID=0):
+        self.sensors = [TCRT5000(pin, chipID) for pin in pins]
+
+    def calibrate(self, duration=1):
+        threads = []
+        for i in range(len(self.sensors)):
+            threads.append(threading.Thread(target=self.sensors[i].calibrate))
+
+            threads[i].start()
+
+        for thread in threads:
+            thread.join()
+
+    @property
+    def values(self):
+        return [sensor.value for sensor in self.sensors]
+
+def single_TCRT5000_test(pin: int, chipID=0):
+    sensor = TCRT5000(pin, chipID)
+    sensor.calibrate()
 
     while True:
-        print(s.value)
+        print(sensor.value)
+
+def multiple_TCRT5000_test(pins: list[int], chipID=0):
+    sensors = multipleTCRT5000(pins, chipID)
+    sensors.calibrate()
+
+    while True:
+        print(sensors.values)
+
+if __name__ == "__main__":
+    multiple_TCRT5000_test([4, 5, 1])
