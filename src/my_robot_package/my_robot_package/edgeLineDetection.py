e62d43685c4fbf5e53a69081b74f16c373c85185
superstrika <superstrika7046@gmail.com>
2026-02-16 12:08:39 +0200

    Feat: added base irq

--- src/my_robot_package/my_robot_package/edgeLineDetection.py
+++ src/my_robot_package/my_robot_package/edgeLineDetection.py
@@ -1,22 +1,27 @@
 import motor
 import data
 from time import sleep
+import lgpioIRQ
 
 class EdgeLineDetection:
     def __init__(self):
         self.motors = motor.multipleMotors(data.MOTOR_PINS)
+        self.irq = lgpioIRQ.IRQ(data.TCRT_PINS, [self.escapeLeft, self.escapeRight, self.escapeForward])
 
     def escapeLeft(self):
+        print("Escaping left!")
         self.motors.setSpeedVxVy(-100, 0)
         sleep(0.1)
         self.motors.setSpeedVxVy(0, 0)
 
     def escapeRight(self):
+        print("Escaping right!")
         self.motors.setSpeedVxVy(100, 0)
         sleep(0.1)
         self.motors.setSpeedVxVy(0, 0)
 
     def escapeForward(self):
+        print("Escaping forward!")
         self.motors.setSpeedVxVy(0, 100)
         sleep(0.1)
         self.motors.setSpeedVxVy(0, 0)
