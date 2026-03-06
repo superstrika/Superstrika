54176353cbe04f85aceed9e15f4fd6f6cbd70661
Noam Ron <146370694+NoamRon1@users.noreply.github.com>
2026-03-06 17:45:01 +0200

    Feat: added base dribbler class: waiting for testing

--- src/my_robot_package/my_robot_package/dribbler.py
+++ src/my_robot_package/my_robot_package/dribbler.py
@@ -1,4 +1,15 @@
 import motor
+import data
+
+class Dribbler:
+    def __init__(self, pins: list[int], chipID: int = 0):
+        self.dribbler = motor.motor7046(pins[0], pins[1], chipID=chipID)
+
+    def turn_on(self, speed: int = 100):
+        self.dribbler.speed = speed
+
+    def turn_off(self):
+        self.dribbler.speed = 0
 
 def dribbler_test():
     dribbler = motor.motor7046(13, 16)  # Example GPIO pins
